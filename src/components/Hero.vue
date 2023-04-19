0173674fa1904affb64b74aafb5eb0db4f624142
jannunen <jarmo@annunen.fi>
2023-04-19 19:33:28 +0300

    ADdead header image

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,14 +2,17 @@
 
 </script>  
 <template>
-  <div class="mt-2 md:mb-16 px-8 py-2 " style="background-color : #EEE3C5">
+  <div class="md:mt-2 md:mb-8 px-8 py-2 " style="background-color : #EEE3C5">
     <div class="flex flex-col items-center justify-center mt-24">
+      <img src="/assets/images/pepeheader.jpg" width="800" class="mx-auto my-4 md:mb-12" />
       <h1 class="font-bold text-4xl my-2">Pepe City: A new Resort Destination in El Salvador</h1>
 
       <p class="text-xl w-4/5 text-gray-500 my-2">
         Pepe City aims to become a luxurious and sustainable resort in El Salvador that offers unique investment opportunities through its connection with cryptocurrency market.
       </p>
+        <!--
       <img src="/assets/images/pepecityhero.jpg" width="400" height="400" class="mx-auto my-2" />
+        -->
 
     </div>
   </div>
