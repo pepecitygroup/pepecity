44d309af7e287ca379b048913e50b79347aeaccb
jannunen <jarmo@annunen.fi>
2023-04-19 20:32:14 +0300

    Changed Team intro

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -2,6 +2,23 @@
 <template>
   <div class="mt-12  px-8 py-4 md:py-14" style="background-color : #EEE3C5">
     <a name="team"><h1 class="text-center text-black font-bold text-4xl my-2">Team</h1></a>
+
+    <p class="my-4">
+The idea for Pepe City was born out of a group of crypto enthusiasts who wanted to create a space where people could come together to enjoy the benefits of cryptocurrency in a relaxed and luxurious environment. The founders of Pepe City saw an opportunity to create a unique experience that would appeal to both cryptocurrency investors and travelers looking for a unique vacation destination.
+      </p>
+
+      <p class="my-4">
+To bring their vision to life, the founders of Pepe City created a digital asset called the Pepe City Coin. This cryptocurrency will be used by guests to pay for various services at the resort, including accommodations, dining, and activities.
+      </p>
+
+      <p class="my-4">
+One of the key features of the Pepe City Coin is its DAO (decentralized autonomous organization). This allows coin holders to have a say in how the resort is run and how the coin is used. By participating in the DAO, investors can help shape the future of the resort and potentially increase the value of their investment.
+      </p>
+
+      <p class="my-4">
+The founders of Pepe City believe that their resort offers a unique investment opportunity for those interested in both cryptocurrency and luxury travel.
+      </p>
+
     <div class="grid grid-cols-1 md:grid-cols-4 gap-2">
       <div>
         <h1 class="text-black font-bold text-1xl my-2">Front dev </h1>
