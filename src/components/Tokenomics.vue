f77a138c04496784568feb3d76d47d3eb3102bbe
jannunen <jarmo@annunen.fi>
2023-04-19 23:22:34 +0300

    Updated tokenomics

--- src/components/Tokenomics.vue
+++ src/components/Tokenomics.vue
@@ -5,9 +5,15 @@
   <div class="px-8 md:py-4 ">
   <a name="tokenomics"><h1 class="text-center font-bold text-2xl md:text-4xl">Tokenomics</h1></a>
   <div class="my-4 grid grid-cols-1 md:grid-cols-1  gap-4">
-    <div>
-        Max. total supply is 420,690,000,000,000 tokens. In order to guarantee that the tokenomics system is working properly, we have a 2% sell and 1% buy 
-        tax. 1% goes to liquidity. These might change in the future, but this is what we have set in place for now.
+    <div class="text-center">
+        MAX TOTAL SUPPLY
+        <br />
+        420,234,000,000,000
+        <br />
+        PPC
+        <br />
+        <br />
+        2% buy / 2% sell (land tax) + 1% auto liquidity
     </div>
   </div>
   </div>
