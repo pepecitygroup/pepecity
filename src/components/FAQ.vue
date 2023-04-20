88c4b107a5b6d94e98994df0275012e0509167af
jannunen <jarmo@annunen.fi>
2023-04-20 14:34:36 +0300

    Added FAQ item, removed wp for now

--- src/components/FAQ.vue
+++ src/components/FAQ.vue
@@ -4,28 +4,28 @@
   <div class="mt-12 mb-12 my-2 px-8 py-4 md:py-14" style="background-color : #EEE3C5">
     <h1 class="text-center font-bold text-4xl my-4">FAQ</h1>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-       <div>
-          <h1 class="font-bold text-2xl my-2">What is Pepe City?</h1>
-          <p class="text my-2">
+      <div>
+        <h1 class="font-bold text-2xl my-2">What is Pepe City?</h1>
+        <p class="text my-2">
           Pepe City is a future resort in El Salvador where investors can buy real estate and holders of PPC receive a share of the resort's income. Holders and top holders of PPC will enjoy lifetime discounts and access.
-
-              
-          </p>
-          <h1 class="font-bold text-2xl my-2">Why Pepe City?</h1>
-          <p class="text my-2">
+        </p>
+        <h1 class="font-bold text-2xl my-2">Why Pepe City?</h1>
+        <p class="text my-2">
           Investing in Pepe City offers a unique opportunity to earn a revenue share from resort income and sales, while also creating jobs for the local community in El Salvador and contributing to the growth of cryptocurrency.
-
-
-          </p>
-       </div>
-
-       <div>
-          <h1 class="font-bold text-2xl my-2">What does Pepe City offer?</h1>
-          <p class="text my-2">
+        </p>
+      </div>
+      <div>
+        <h1 class="font-bold text-2xl my-2">What does Pepe City offer?</h1>
+        <p class="text my-2">
           Pepe City offers a plethora of experiences, from accommodation and dining to activities, exclusive events, and the use of PPC at the resort, all while supporting the local community and the growth of cryptocurrency.
-
-          </p>
-       </div>
+        </p>
+      </div>
+      <div>
+        <h1 class="font-bold text-2xl my-2">When will the land purchase be done?</h1>
+        <p class="text my-2">
+          We'll buy the land for Pepe City once our trading taxes generate enough revenue, which we expect to happen quickly.
+        </p>
+      </div>
     </div>
   </div>
 </template>
