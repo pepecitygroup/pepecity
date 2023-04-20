2e2d69c3df4b056d549bd7ab2493fafe0e69f7f3
jannunen <jarmo@annunen.fi>
2023-04-21 01:08:25 +0300

    Added buy/chart links

--- src/components/About.vue
+++ src/components/About.vue
@@ -18,8 +18,8 @@
         If you are interested in learning more about Pepe City and its associated Pepe City Coin, we encourage you to visit our website and explore our website not 1, not 2 but 100 times. You can also join our community on social media and participate in discussions with other like-minded individuals. We welcome anyone who shares our vision of creating sustainable resorts and infrastructure while providing an innovative payment solution. Together, we can make Pepe City a reality.
       </p>
       <div class="my-2 mt-8 flex flex-wrap">
-        <a class="m-1 bg-black text-gray-100 px-8 py-3 lg font-bold text-lg rounded-lg hover:bg-gray-800">Buy</a>
-        <a class="m-1 bg-black text-gray-100 px-8 py-3 lg font-bold text-lg rounded-lg hover:bg-gray-800">Charts</a>
+        <a href="https://app.uniswap.org/#/swap?outputCurrency=0x01e006708440ea4ec259869232b4127089e9753d" target="_blank" class="m-1 bg-black text-gray-100 px-8 py-3 lg font-bold text-lg rounded-lg hover:bg-gray-800">Buy</a>
+        <a href="https://dexscreener.com/ethereum/0xc5b4aa3383b56872ff06c8ece9f26e815f095cef"  target="_blank" class="m-1 bg-black text-gray-100 px-8 py-3 lg font-bold text-lg rounded-lg hover:bg-gray-800">Charts</a>
         <a href="/assets/tmp.pdf" target="_blank" class="m-1 bg-black text-gray-100 px-8 py-3 lg font-bold text-lg rounded-lg hover:bg-gray-800">Whitepaper</a>
       </div>
     </div>
