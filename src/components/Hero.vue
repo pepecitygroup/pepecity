f90fabf8659b8185b4d51163b42b448d5785112a
jannunen <jarmo@annunen.fi>
2023-04-20 19:46:06 +0300

    Added a little transition

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,37 @@
 <script setup>
+import { ref, computed } from 'vue'
+const isElevated = ref(false)
+const getClass = computed(() => {
+  if (isElevated.value) {
+    return 'mx-auto img-elevated-size img-default-size'
+  }
+  return 'mx-auto img-default-size'
+})
 
 </script>  
 <template>
   <div class="md:mt-2 md:mb-8 px-8 py-2 " style="background-color : #EEE3C5">
     <div class="flex flex-col items-center justify-center mt-24">
-      <img src="/assets/images/pepeheader.jpg" width="800" class="mx-auto my-4 md:mb-12" />
+      <div style="height : 400px">
+        <img src="/assets/images/pepeheader.jpg" width="800" :class="getClass" @mouseover="isElevated= true" @mouseout="isElevated = false" />
+      </div>
       <h1 class="font-bold text-4xl my-2">Pepe City: A new Resort Destination in El Salvador</h1>
 
       <p class="text-xl w-4/5 text-gray-500 my-2">
         Pepe City aims to become a luxurious and sustainable resort in El Salvador that offers unique investment opportunities through its connection with cryptocurrency market.
       </p>
-        <!--
-      <img src="/assets/images/pepecityhero.jpg" width="400" height="400" class="mx-auto my-2" />
-        -->
-
     </div>
   </div>
 </template>
+<style>
+.img-default-size {
+height: 320;
+transition: all 0.3s;
+}
+
+.img-elevated-size {
+transition: all 0.3s;
+box-shadow: 6px 6px 20px rgb(66, 66, 66);
+transform: rotate(-2deg);
+}
+</style>
